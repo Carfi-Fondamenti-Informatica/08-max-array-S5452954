9ca0446da87069a56998346956217be401ee76ff
Giovanni Gemme 5452954 <115399060+S5452954@users.noreply.github.com>
2022-12-11 17:19:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+
+
 
 int main(){
-  return 0;
+
+    int dimensione = 0;
+    cin >> dimensione;
+    float numeri [dimensione];
+
+    for (int i=0; i<dimensione; i++) {
+        cin >> numeri[i];
+    }
+
+    float risultato = funzione (numeri, dimensione);
+    cout << risultato;
+
+    return 0;
 }
